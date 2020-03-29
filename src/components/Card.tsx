c538b3cb2ade12ac58b3d8b196433b6badaeca09
Marc Bakker <webdev@marcbakker.com>
2020-03-29 18:15:55 +0200

    Added JSDoc; Callback 'updateCardReference()' now has 1 object parameter containing 2 functions instead of 1 function; Use state property 'imgPath' instead of the component property to display cardImage.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,16 +12,27 @@ const Card = ({
 	updateCardReference: Function;
 }) => {
 	const [flippedState, setFlippedState] = useState(false);
+	const [imagePath, setImagePath] = useState(card.imgPath);
 
 	useEffect(() => {
-		// give Board comp a reference to the flipCard function
-		updateCardReference(flipCard);
-	}, [updateCardReference]);
+		// give Board component a reference to the flipCard and setImagePath functions
+		updateCardReference({ flipCard: flipCard, setImagePath: setImagePath });
+	}, [imagePath, updateCardReference]);
 
+	/**
+	 * @function flipCard
+	 * @description function to turn a card upside down
+	 * @param {boolean} newState - true: card turned backside up, false card turned backside down
+	 * @return {void}
+	 */
 	function flipCard(newState: boolean) {
 		setFlippedState(newState);
 	}
 
+	/**
+	 * @function onClick
+	 * @description function executed when clicked on a card.. Calls flipCard() to flip card and onHandleClick() to invoke Board component related functionality
+	 */
 	const onClick = () => {
 		flipCard(true);
 		onHandleClick({
@@ -36,10 +47,10 @@ const Card = ({
 		<div className='scene' data-id={card.id} data-row={card.row} data-col={card.col} onClick={onClick}>
 			<div className={"card " + (flippedState ? "is-flipped" : "")}>
 				<div className='card__face card__face--front'>
-					{card.visible ? <img src={card.imgPath} alt='' /> : <img src={card.hiddenImgPath} alt='' />}
+					{card.visible ? <img src={imagePath} alt='' /> : <img src={card.hiddenImgPath} alt='' />}
 				</div>
 				<div className='card__face card__face--back'>
-					{!card.visible ? <img src={card.imgPath} alt='' /> : <img src={card.hiddenImgPath} alt='' />}
+					{!card.visible ? <img src={imagePath} alt='' /> : <img src={card.hiddenImgPath} alt='' />}
 				</div>
 			</div>
 		</div>

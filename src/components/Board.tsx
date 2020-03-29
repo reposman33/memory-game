9d130ddc495cdfd06ff2e74aa57bf8c03f422e10
Marc Bakker <webdev@marcbakker.com>
2020-03-29 19:03:11 +0200

    Changed Board Component to accomodate for object parameter in onClick(); Rename fileNames to fileNamesArray; Added state fileNamesArray to convert object fileNames to array in useEffect.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,57 +3,60 @@ import { Row } from "./Row";
 import { TCard } from "../types/Card";
 import "./board.scss";
 
-type card = {
+type TClickedcard = {
 	row: number;
 	col: number;
 	id: number;
 	flip: Function;
 };
+
+type cardReference = {
+	flipCard: Function;
+	setImagePath: Function;
+};
+
 const Board = () => {
-	const [fileList, setFileList] = useState<{}>();
+	const [fileNamesArray, setFileNamesArray] = useState<string[]>([]);
 	const nrOfColumns = 8;
 	const assetsPath = "/assets/img";
-	let turnedCards: card[] = [];
-	const cards: Function[] = [];
+	let turnedCards: TClickedcard[] = [];
+	const cardReferences: cardReference[] = [];
 
 	useEffect(() => {
 		fetch(`${assetsPath}/cards/files.json`)
 			.then(res => res.json())
 			.then(res => {
-				setFileList(res);
+				const _arr = Object.keys(res).concat(Object.keys(res));
+				setFileNamesArray(_arr);
 			})
 			.catch(e => console.log("ERROR: ", e));
 	}, []);
 
-	const onClickCard = (clickedCard: card) => {
-		if (turnedCards.some(card => card.id === clickedCard.id)) {
-			return;
-		}
-		if (turnedCards.length === 2) {
-			turnedCards.forEach(card => card.flip(false));
-			turnedCards = [];
-			turnedCards.push(clickedCard);
-		} else if (turnedCards.length === 1) {
-			turnedCards.push(clickedCard);
-			// compare card equality
-		} else {
-			turnedCards.push(clickedCard);
-		}
-	};
-
-	const getMemoryCards = (fileData: {}, imgPath: string) => {
-		const fileNames: string[] = Object.keys(fileData).concat(Object.keys(fileData));
-		return fileNames.map((fileName: string, i: number) => ({
+	/**
+	 * @function getMemoryCards create an array of filenames
+	 * @param {object} fileData - array containing filenames of cards: ["a04.jpg","b01.jpg",... ] from  filenames.json
+	 * @param {string} imgPath - path to img folder
+	 * @returns {array} - an array of TCard objects
+	 */
+	const getMemoryCards = (fileData: {}, imgPath: string): TCard[] => {
+		return fileNamesArray.map((fileName: string, i: number) => ({
 			col: i % nrOfColumns,
 			id: i,
 			hiddenImgPath: `${assetsPath}/hiddenImg.jpg`,
 			imgPath: `${assetsPath}/cards/${fileName}`,
 			onClickCard: onClickCard,
 			row: Math.floor(i / nrOfColumns),
-			visible: false
+			visible: true
 		}));
 	};
 
+	/**
+	 * @function makeRows - make <nrOfColumns> Row components containing <nrOfColumns> cards
+	 * @param {array} memoryCards - array of memorycards
+	 * @param {number} nrOfColumns - the number of columns. Since the board is rectangular this is also equal to the number of rows.
+	 * @param {Function} onClickCard - callback that a card executes when clicked.
+	 * @returns an array with Row components
+	 */
 	const makeRows = (memoryCards: TCard[], nrOfColumns: number, onClickCard: Function) => {
 		const rows = [];
 		for (let i = 0; i < memoryCards.length; i++) {
@@ -67,19 +70,47 @@ const Board = () => {
 		return rows;
 	};
 
-	// add ref to flipCard function from cards
-	const updateCardReference = (f: Function) => {
-		cards.push(f);
+	/**
+	 * @function onClickCard - Callback invoked by Card component. It invokes functionality such as flipping back cards when the 3d card is clicked, checking card equality etc
+	 * @param {TClickedcard} the clickedCard containing the flip function to turn the card upside down.
+	 * @returns {void}
+	 */
+	const onClickCard = (clickedCard: TClickedcard) => {
+		if (turnedCards.some(card => card.id === clickedCard.id)) {
+			return;
+		}
+		if (turnedCards.length === 2) {
+			turnedCards.forEach(card => card.flip(true));
+			turnedCards = [];
+			turnedCards.push(clickedCard);
+		} else if (turnedCards.length === 1) {
+			turnedCards.push(clickedCard);
+			// compare card equality
+		} else {
+			turnedCards.push(clickedCard);
+		}
 	};
 
+	/**
+	 * @function updateCardReference - callback for Card executed after rendering returning references to setState functions
+	 * @param {object} refs - an object where each value refers to a function of the same name
+	 * @returns {void}
+	 */
+	const updateCardReference = (refs: cardReference) => {
+		cardReferences.push(refs);
+	};
+
+	/**
+	 * @function onStart - executed when start button clicked. Hides and shuffles cards
+	 */
 	const onStart = () => {
-		cards.forEach(flipCard => flipCard(true));
+		cardReferences.forEach(ref => ref.flipCard(true));
 	};
 
 	return (
 		<div className='container'>
 			<div className='board'>
-				{fileList && makeRows(getMemoryCards(fileList, `${assetsPath}`), nrOfColumns, onClickCard)}
+				{fileNamesArray && makeRows(getMemoryCards(fileNamesArray, `${assetsPath}`), nrOfColumns, onClickCard)}
 			</div>
 			<button className='button' onClick={onStart}>
 				Start

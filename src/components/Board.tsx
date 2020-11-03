8df83594cb0d6edcbf758918928e69822580673e
Marc Bakker <webdev@marcbakker.com>
2020-11-03 18:31:01 +0100

    Revert "Add an option to select the size of the board - 4*4,6*6 or 8*8."
    This reverts commit 4521fdde35db8f4c27b257dfb3ee7a884b9e0d8b.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -18,8 +18,7 @@ type cardReference = {
 };
 
 type TButtonStatus = "ACTIVE" | "INACTIVE" | "DEMO";
-// Size of board. nrOfColumns === nrOfRows, we have 4*4 and 6*6 matrix since total Nr OfCards has to be even to have pairs of equal cards
-const nrOfColumns = 6; // === (4*4) / 2 unique cards; (6*6) / 2 = 18 unique cards; (8*8) / 2 = 32 unique cards
+const nrOfColumns = 8;
 const assetsPath = "/assets/img";
 const cardReferences: cardReference[] = [];
 let turnedCards: TClickedcard[] = [];
@@ -30,7 +29,6 @@ let gameActive: boolean = false;
 const Board = () => {
 	const [fileNamesArray, setFileNamesArray] = useState<string[]>([]);
 	// Scoreboard dependencies
-	const [boardSize, setBoardSize] = useState(nrOfColumns);
 	const [score, setScore] = useState(0);
 	const [moveCount, setMoveCount] = useState(0);
 	const [scoreBoardVisibility, setScoreBoardVisibility] = useState(false);
@@ -39,25 +37,17 @@ const Board = () => {
 	const [buttonStatus, setButtonStatus] = useState<TButtonStatus>("ACTIVE");
 	const [language, setLanguage] = useState(I18n.language);
 
-/**
- * @description 1a retrieve the names of all the _unique_ cardImages;
- * 				1b determine the number of unique images;
- * 				1c duplicate that so we have all the card images needed for the board;
- */
 	useEffect(() => {
-		setFileNamesArray([]); // start with an empty board
-		fetch(`${assetsPath}/cards/files.json`) //1
+		fetch(`${assetsPath}/cards/files.json`)
 			.then((res) => res.json())
 			.then((res) => {
-				const nrOfUniqueCards = Math.pow(boardSize, 2) / 2; // 2
-				const cardfileNames = Object.keys(res).slice(0, nrOfUniqueCards); // 2
-				setFileNamesArray(cardfileNames.concat(cardfileNames)); // 3
+				const _arr = Object.keys(res).concat(Object.keys(res));
+				setFileNamesArray(_arr);
 			})
 			.catch((e) => console.log("ERROR: ", e));
-	}, [boardSize]);
+	}, []);
 
 	/**
-	 * @description - 2 convert the array of cardImage names to an objectarray 
 	 * @function getMemoryCards
 	 * @description create an array of filenames
 	 * @param {array} imgNames - array containing filenames of cards: ["a04.jpg","b01.jpg",... ] from  filenames.json
@@ -66,31 +56,33 @@ const Board = () => {
 	 */
 	const getMemoryCards = (imgNames: string[], imgPath: string): TCard[] => {
 		return imgNames.map((fileName: string, i: number) => ({
-			col: i % boardSize,
+			col: i % nrOfColumns,
 			id: i,
 			hiddenImgPath: `${assetsPath}/hiddenImg.jpg`,
 			imgPath: `${assetsPath}/cards/${fileName}`,
 			onClickCard: onClickCard,
-			row: Math.floor(i / boardSize),
+			row: Math.floor(i / nrOfColumns),
 			visible: true,
 		}));
 	};
 
 	/**
 	 * @function makeRows
-	 * @description 3 Initialize boardSize Row components containing each boardSize cards
+	 * @description make <nrOfColumns> Row components containing <nrOfColumns> cards
 	 * @param {array} memoryCards - array of memorycards
-	 * @param {number} boardSize - the number of columns. Since the board is rectangular this is also equal to the number of rows.
+	 * @param {number} nrOfColumns - the number of columns. Since the board is rectangular this is also equal to the number of rows.
 	 * @param {Function} onClickCard - callback that a card executes when clicked.
 	 * @returns an array with Row components
 	 */
-	const makeRows = (memoryCards: TCard[], boardSize: number, onClickCard: Function) => {
+	const makeRows = (memoryCards: TCard[], nrOfColumns: number, onClickCard: Function) => {
 		const rows = [];
-		for (let row = 0; row < boardSize; row++) {
-				const _cards = memoryCards.slice(row * boardSize, row * boardSize + boardSize);
+		for (let i = 0; i < memoryCards.length; i++) {
+			if (i % nrOfColumns === 0) {
+				const _cards = memoryCards.slice(i, i + nrOfColumns);
 				rows.push(
-					<Row key={row} cards={_cards} onClick={onClickCard} updateCardReference={updateCardReference} />
+					<Row key={i} cards={_cards} onClick={onClickCard} updateCardReference={updateCardReference} />
 				);
+			}
 		}
 		return rows;
 	};
@@ -185,17 +177,8 @@ const Board = () => {
 				scoreBoardVisibility={scoreBoardVisibility}
 				gameOverText={gameOverText}
 			/>
-			<div className="subHeader">Choose a board size</div>
-				<div>
-					<input id="boardSize8" type="radio" name="boardsize" value="8" checked={boardSize===8} onClick={()=>setBoardSize(8)} />
-					<label htmlFor="boardSize8">8*8</label>
-					<input id="boardSize6" type="radio" name="boardsize" value="6" checked={boardSize===6} onClick={()=>setBoardSize(6)} />
-					<label htmlFor="boardSize6">6*6</label>
-					<input id="boardSize4" type="radio" name="boardsize" value="4" checked={boardSize===4} onClick={()=>setBoardSize(4)} />
-					<label htmlFor="boardSize4">4*4</label>
-				</div>
 			<div className='board'>
-				{fileNamesArray && makeRows(getMemoryCards(fileNamesArray, `${assetsPath}`), boardSize, onClickCard)}
+				{fileNamesArray && makeRows(getMemoryCards(fileNamesArray, `${assetsPath}`), nrOfColumns, onClickCard)}
 			</div>
 			<Button status={buttonStatus} onStart={onStart} />
 		</div>
